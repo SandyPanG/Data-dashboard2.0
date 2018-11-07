9337590ed0ad8a2243c70f211f6a68a7c0293c77
SandyPanG <soleibiza@hotmail.com>
2018-11-06 21:40:15 -0600

    scouting one

--- js/app.js
+++ js/app.js
@@ -3,4 +3,16 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+// console.log(data);
+
+var seat = Object.keys(data);
+console.log(Object.keys(data));//sedes
+
+// console.table(data);
+
+for (var i = 0; i < seat.length; i++) {
+    // data[i].students[2];
+    console.log(Object.keys(data[seat[i]]));
+    
+}
+
