5252e33d66effeedfa821de006e3160350bc3b2c
SandyPanG <soleibiza@hotmail.com>
2018-11-26 22:55:57 -0600

    dropdown in overview

---
+++ js/overview.js
@@ -0,0 +1,8 @@
+//cargar opción elegida y marcarla cómo activa
+window.addEventListener('load', function() {
+    var btnDefault = document.getElementById('activo');
+    btnDefault.className += "activo";
+});
+
+// Elemento Dropdown Sedes
+var sede = document.getElementById('sede-overview');
